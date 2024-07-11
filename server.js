bb0d76503096762e0c6080a0c94c1541a5e75659
DESKTOP-H76IBH5\USER <404.ginda@gmail.com>
2024-07-11 23:32:03 +0700

    asd

---
+++ server.js
@@ -0,0 +1,35 @@
+const { SMTPServer } = require("smtp-server");
+
+const server = new SMTPServer({
+  name: "mail.bageur-bablast.my.id",
+  onAuth(auth, session, callback) {
+    if (auth.username === "user" && auth.password === "password") {
+      callback(null, { user: "user" });
+    } else {
+      callback(new Error("Authentication failed"));
+    }
+  },
+  onData(stream, session, callback) {
+    let email = "";
+    stream.on("data", (chunk) => {
+      email += chunk.toString();
+    });
+    stream.on("end", () => {
+      console.log("Received email:", email);
+      callback(null); // Accept the message
+    });
+  },
+  onMailFrom(address, session, callback) {
+    console.log("Mail from:", address.address);
+    callback();
+  },
+  onRcptTo(address, session, callback) {
+    console.log("Mail to:", address.address);
+    callback();
+  },
+  logger: true,
+});
+
+server.listen(25, () => {
+  console.log("SMTP server is listening on port 25");
+});
